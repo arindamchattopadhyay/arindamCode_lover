c41ceaa7ae89723e5d4748bc65bf1553dfc15893
arindamchattopadhyay <40838263+arindamchattopadhyay@users.noreply.github.com>
2020-08-30 02:44:55 +0530

    Update Merge_Sorted_Constant_Space.java

--- Merge_Two_Sorted_Arrays/Merge_Sorted_Constant_Space.java
+++ Merge_Two_Sorted_Arrays/Merge_Sorted_Constant_Space.java
@@ -1,5 +1,25 @@
 //Merge two sorted arrays in O(1) space.
+/*
+Here we have two sorted arrays but we can have higher value elements in the 1st array and lower value elements in the 2nd array.
+Aim:- We need to make sure that the 1st array should contain the lower values and the 2nd one with higher value elements.
+To do that, we form two bitonic arrays. i.e (1st increasing and then decreasing)
 
+And then the sort each array using shell sort technique of using gap and thereby making it better than insertion sort.
+Input#1:- 
+5
+4
+1 2 7 9 12
+3 5 11 13
+Prior sorting,
+we will get two arrays:- 
+arr1:- {1,2,7,5,3}
+arr2:- {12,9,11,13}
+So, there's no element in the 1st array greater than any element in the 2nd array.
+Now it's simple.
+Just a sort of each arrays and done by reducing the gap after each iteration.
+@author: Arindam Chattopadhay
+Date:- 30/08/2020
+*/
 import java.util.*;
 public class Merge_Sorted_Constant_Space{
 	private static int nextGap(int gap)
