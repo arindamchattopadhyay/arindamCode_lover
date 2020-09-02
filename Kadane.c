8f8aa4093103d9b833c56dccda6ca5253d50d7fa
arindamchattopadhyay <40838263+arindamchattopadhyay@users.noreply.github.com>
2020-09-03 02:43:43 +0530

    Update Kadane.c
    Kadane's Algorithm

--- Kadane.c
+++ Kadane.c
@@ -55,3 +55,75 @@ void kadane(int arr[],int n)
 		printf(" %d\t",arr[i]);
 	}
 }
+//Kadane's Algorithm
+//We are required to find the largest sum contiguous array.
+import java.util.*;
+public class KadaneAlgo{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		//O(n^3)
+		/*
+		int max=0;
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				int sum=0;
+				for(int k=i;k<j;k++)
+				{
+					sum+=a[k];
+
+				}
+				max=Math.max(max,sum);
+			}
+		}
+		System.out.println(max);
+		o(n^2)*/
+		int max=Integer.MIN_VALUE;
+		for(int i=0;i<n;i++)
+		{
+			int currmax=0;
+			for(int j=i;j<n;j++)
+			{
+					currmax=Math.max(a[j],a[j]+currmax);
+
+					max=Math.max(currmax,max);
+			}
+		}
+		System.out.println(max);
+		/*int max=0,currmax=0,sum=0,s=0,end=0,start=0;
+		for(int i=1;i<n;i++){
+			currmax+=a[i];
+			if(max<currmax)
+			{
+				max=currmax;
+				start=s;
+				end=i;
+			}
+			if(currmax<0)
+			{
+				currmax=0;
+				s=i+1;
+			}
+
+
+			
+		}
+		System.out.println(max+" Start index:- "+start+" End index:- "+(end));*/
+	}
+}
+/*
+
+IP:- 
+5
+9 -8 -1 -7 2
+8
+-2 -3 4 -1 -2 1 5 -3
+*/
